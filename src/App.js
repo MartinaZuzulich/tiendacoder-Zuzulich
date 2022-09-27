21fd66507a319958583047f0b37f8cc6883005e2
MartinaZuzulich <109553633+MartinaZuzulich@users.noreply.github.com>
2022-09-26 23:18:38 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './componets/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      {/* <h1>Hola mundo</h1> */}
+    </div>
+  );
+}
+
+export default App;
